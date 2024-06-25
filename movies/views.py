c8eda26899eec54159811671305fc63b4c43b353
Anonymous <anonymous@example.com>
2024-06-25 17:21:23 +0530

    Add Templates and Bootstrap.

--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello world!!!")
+    movies = Movie.objects.all()
+    return render(request, 'movies/index.html', {'movies': movies})
